1e94949929dc8df94fe8d7ce7cdf7c752a74b64d
Thomas Schöb <t.schoeb@iwv3.kfa-juelich.de>
2021-11-18 12:57:03 +0100

    Added a function for the power of two numbers

--- printNumbers/unittests/test_power.py
+++ printNumbers/unittests/test_power.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# runTestSuite.py
+# test_power.py
 #
 # This file is part of PrintNumbers.
 #
@@ -20,26 +20,36 @@
 # along with PrintNumbers.  If not, see <http://www.gnu.org/licenses/>.
 
 #
-# Run unit test suite.
+# Unit tests: 'power'.
 #
 
-import unittest
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-import test_factorial
-import test_fibonacci
-import test_power
+import unittest
+from functions.power import *
+
+class TestPower(unittest.TestCase):
+
+    def test_value_0(self):
+        self.assertEqual(Power(1,0), 1)
+
+    def test_value_1(self):
+        self.assertEqual(Power(2,2), 4)
+
+    def test_value_2(self):
+        self.assertEqual(Power(2,3), 8)
+
+
 
 
 def suite():
-    suite = unittest.TestSuite()
-
-    suite.addTest(test_factorial.suite())
-    suite.addTest(test_fibonacci.suite())
-    suite.addTest(test_power.suite())
-
+    suite = unittest.makeSuite(TestPower, 'test')
     return suite
 
-
-if __name__ == "__main__":
+def run():
     runner = unittest.TextTestRunner(verbosity = 2)
     runner.run(suite())
+
+if __name__ == "__main__":
+    run()
